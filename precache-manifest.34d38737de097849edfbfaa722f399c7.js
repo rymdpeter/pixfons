3bda4c34e52e52ad744e5e7708377ce1e90a96aa
Peter Karlsson <peter@esatto.se>
2018-10-18 16:16:54 +0200

    Inital commit

---
+++ precache-manifest.34d38737de097849edfbfaa722f399c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c1758f6b9d5b861fbee2",
+    "url": "/pixfons/static/js/runtime~main.c1758f6b.js"
+  },
+  {
+    "revision": "c1c22a69057ec88259ee",
+    "url": "/pixfons/static/js/main.c1c22a69.chunk.js"
+  },
+  {
+    "revision": "13a15114ae3ac597de25",
+    "url": "/pixfons/static/js/1.13a15114.chunk.js"
+  },
+  {
+    "revision": "c1c22a69057ec88259ee",
+    "url": "/pixfons/static/css/main.d40a9bfd.chunk.css"
+  },
+  {
+    "revision": "bbc6a0692f2e79aa3694b5e51e40a195",
+    "url": "/pixfons/index.html"
+  }
+];
